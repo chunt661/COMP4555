f4e000c0be468eb953a33f22d9cd1bdfabcf8469
cwis <chunt661@mtroyal.ca>
2022-04-04 08:51:20 -0600

    Added 'enemy killed' event

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
     // How many times should I be hit before I die
     public int health = 2;
+    private EnemySpawner spawner;
+
+    void Start()
+    {
+        spawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -18,10 +25,12 @@ public class Damage : MonoBehaviour
         {
             health =- 2;
 
-        if (health <= 0)
-        {
-            Destroy(this.gameObject);
+            if (health <= 0)
+            {
+                Destroy(this.gameObject);
+                //spawner.OnEnemyKilled();
+                spawner.SendMessage("OnEnemyKilled");
+            }
         }
     }
-}
 }
